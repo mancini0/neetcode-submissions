220e150e22b170b219658d430b6a8688bf3ad8ba
mike mancini <mancini0@gmail.com>
2026-06-25 14:50:24 -0400

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.go
@@ -0,0 +1,23 @@
+import (
+	"slices"
+)
+func maxSlidingWindow(nums []int, k int) []int {
+	window:= make([]int,k,k)
+	l:=0
+	copy(window[:],nums[l:k])
+	res:=make([]int,0,(len(nums)/k)+len(nums)%k)
+	if k == len(nums){
+		res = append(res, slices.Max(nums))
+		return res
+	}
+	for r:=k; r<=len(nums);r++{
+		res = append(res, slices.Max(window))
+		l++
+		for i:= range k{
+			if l+i<len(nums){
+				window[i]=nums[l+i]
+			}
+		}
+	}
+	return res
+}
